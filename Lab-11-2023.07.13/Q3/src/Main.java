4f4ad8c7761a2c6df52413f1111337fa15f772fb
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2023-07-24 21:35:37 +0530

    Q3 updated

--- Lab-11-2023.07.13/Q3/src/Main.java
+++ Lab-11-2023.07.13/Q3/src/Main.java
@@ -1,12 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        char stmt[] = new char[50];
         try{
             File obj = new File("hello.txt");
+            FileReader reader = new FileReader(obj);
+            reader.read(stmt);
         }catch (FileNotFoundException e){
-            System.out.println("hey");
+            System.out.println("file not found");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
